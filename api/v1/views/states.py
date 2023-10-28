07f5dd4275bed0244d953899ce33cb4dcd2bf217
Anzelah <jepkoechanzellah@gmail.com>
2023-10-28 14:02:03 -0700

    beginning task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,24 +2,14 @@
 """Defines the modules"""
 from api.v1.views import app_views
 from models import storage
-from flask import abort
-
+from flask import abort, jsonify
 
 @app_views.route('/states', methods=['GET'])
-def get_states():
-    """retrieves an object into a valid json"""
-    st = storage.all("States")
-    states_dict = st.to_dict()
-    return states_dict
-
-
-@app_views.route('/states/<state_id>', methods=['GET'])
-def useid(state_id):
-    """retrieve a specific state using its id"""
-
+def get():
+    """retrieve all the state objects"""
     st = storage.all("State")
-    if st['id'] == state_id:
-        states_dict = st.to_dict()
-        return states_dict
-    else:
-        abort(404)
+
+    all_states = [] # empty basket
+    for state in st.values():
+        all_states.append(state.to_dict())
+    return jsonify(all_states)
