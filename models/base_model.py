73d3bed4a110feb0decbbb6a4b0b10895355d947
sakachris <sakachris90@gmail.com>
2023-10-29 13:59:20 +0300

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -44,8 +44,8 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+            self.created_at = self.updated_at = datetime.utcnow()
+            # self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
